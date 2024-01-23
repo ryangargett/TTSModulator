e7b012a8c3b06fbf716ef2f51688dd870549e01b
Ryan Gargett <r.d.gargett@gmail.com>
2024-01-23 21:07:02 +0800

    Removed magic values

--- scripts/utils/config.py
+++ scripts/utils/config.py
@@ -1,1 +1,15 @@
+##################################################
+#File: config.py                                 #
+#Project: AdaptiveLLM                            #
+#Created Date: Tue Jan 23 2024                   #
+#Author: Ryan Gargett                            #
+#-----                                           #
+#Last Modified: Tue Jan 23 2024                  #
+#Modified By: Ryan Gargett                       #
+##################################################
+
 VOICE_PRESET = "v2/en_speaker_6"
+PADDING_LENGTH = 0.1
+SEMANTIC_TEMP = 0.6
+MIN_EOS_P = 0.05
+MIN_SENTENCES = 3
